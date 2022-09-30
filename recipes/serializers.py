127cc993ca1373728c43e03b13f8657e93ad6af1
JerberthRocha <jerberthrs@gmail.com>
2022-09-30 16:18:37 -0400

    Migrando para ModelSerializer ao invés de Serializer no Django Rest Framework

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,6 +1,8 @@
+from dataclasses import fields
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from tag.models import Tag
+from .models import Recipe
 
 
 class TagSerializer(serializers.Serializer):
@@ -8,31 +10,39 @@ class TagSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=255)
     slug = serializers.SlugField()
 
+    # class Meta:
+    #     model = Tag
+    #     fields = ['id', 'name', 'slug']
 
-class RecipeSerializer(serializers.Serializer):
+
+class RecipeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipe
+        fields = [
+            'id', 'title', 'description', 'public',
+            'preparation', 'category', 'author', 'tags',
+            'tag_objects', 'tag_links',
+
+            ]
+
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=65)
     description = serializers.CharField(max_length=165)
-    public = serializers.BooleanField(source='is_published')
-    preparetion = serializers.SerializerMethodField()
-    category = serializers.StringRelatedField()
-    author = serializers.PrimaryKeyRelatedField(
-        queryset=User.objects.all()
-    )
-    tags = serializers.PrimaryKeyRelatedField(
-        queryset=Tag.objects.all(),
-        many=True,
-    )
+    public = serializers.BooleanField(source='is_published', read_only=True,)
+    preparation = serializers.SerializerMethodField(read_only=True,)
+    category = serializers.StringRelatedField(read_only=True,)
+
     tag_objects = TagSerializer(
         many=True,
-        source='tags'
+        source='tags',
+        read_only=True,
     )
     tag_links = serializers.HyperlinkedRelatedField(
         many=True,
         source='tags',
-        queryset=Tag.objects.all(),
-        view_name='recipes:recipes_api_v2_tag'
+        view_name='recipes:recipes_api_v2_tag',
+        read_only=True,
     )
 
-    def get_preparetion(self, recipe):
+    def get_preparation(self, recipe):
         return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
