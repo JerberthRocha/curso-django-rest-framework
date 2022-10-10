c35470740d7ea0588932f34b18c87512615beeba
JerberthRocha <jerberthrs@gmail.com>
2022-10-10 17:00:18 -0400

    Filtrando a queryset com get_queryset na ViewSet do Django Rest Framework

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -13,6 +13,27 @@ class RecipeAPIv2ViewSet(ModelViewSet):
     serializer_class = RecipeSerializer
     pagination_class = PageNumberPagination
 
+    def get_serializer_class(self):
+        return super().get_serializer_class()
+
+    def get_serializer(self, *args, **kwargs):
+        return super().get_serializer(*args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["example"] = 'This is in context now'
+        return context
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        category_id = self.request.query_params.get('category_id', '')
+
+        if category_id != '' and category_id.isnumeric():
+            qs = qs.filter(category_id=category_id)
+
+        return qs
+
 
 @api_view()
 def tag_api_detail(request, pk):
