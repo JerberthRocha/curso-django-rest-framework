bea67efc001db4b9448343aa8157f995b59e1372
JerberthRocha <jerberthrs@gmail.com>
2022-10-11 20:58:38 -0400

    Usando request.user no Django Rest Framework para sobrescrever o método create

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -9,6 +9,7 @@ from rest_framework.pagination import PageNumberPagination
 from ..permissions import IsOwner
 from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import status
 
 class RecipeAPIv2ViewSet(ModelViewSet):
     queryset = Recipe.objects.get_published()
@@ -53,7 +54,18 @@ class RecipeAPIv2ViewSet(ModelViewSet):
         if self.request.method in ['PATCH', 'DELETE']:
             return [IsOwner(), ]
         return super().get_permissions()
-
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        serializer.save(author=request.user )
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, 
+            status=status.HTTP_201_CREATED, 
+            headers=headers
+        )
 
 @api_view()
 def tag_api_detail(request, pk):
