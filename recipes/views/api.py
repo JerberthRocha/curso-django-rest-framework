83c61e527f6dc7dad03f2f1c32d9c3e86f33a224
JerberthRocha <jerberthrs@gmail.com>
2022-10-11 20:28:56 -0400

    Criando permissões no Django Rest Framework

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -6,14 +6,15 @@ from django.shortcuts import get_object_or_404
 from tag.models import Tag
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAuthenticated
-
+from ..permissions import IsOwner
+from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class RecipeAPIv2ViewSet(ModelViewSet):
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     pagination_class = PageNumberPagination
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
 
     def get_serializer_class(self):
         return super().get_serializer_class()
@@ -35,6 +36,23 @@ class RecipeAPIv2ViewSet(ModelViewSet):
             qs = qs.filter(category_id=category_id)
 
         return qs
+    
+    def get_object(self):
+        pk = self.kwargs.get('pk', '')
+
+        obj = get_object_or_404(
+            self.get_queryset(),
+            pk=pk,
+        )
+
+        self.check_object_permissions(self.request, obj)
+
+        return obj
+    
+    def get_permissions(self):
+        if self.request.method in ['PATCH', 'DELETE']:
+            return [IsOwner(), ]
+        return super().get_permissions()
 
 
 @api_view()
